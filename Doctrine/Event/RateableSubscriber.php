c8a536337d0d021320bb7a84978b09fad4780928
Ethan Resnick <ethan.resnick@gmail.com>
2012-10-08 11:40:22 -0400

    Work on a Doctrine listener that could automatically update ratings
    This is provisional (totally untested) but won't affect anything else
    negatively. (Should really be in a different branch, but yeah.)

--- Doctrine/Event/RateableSubscriber.php
+++ Doctrine/Event/RateableSubscriber.php
@@ -1,11 +1,13 @@
 <?php
 namespace ERD\RateableEntitiesBundle\Doctrine\Event;
 
-use Doctrine\ORM\Event\OnFlushEventArgs;
 use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
 use Doctrine\ORM\Events;
 use ERD\DoctrineHelpersBundle\Provider\DoctrineEntityProvider;
 use Symfony\Bundle\DoctrineBundle\Registry;
+use ERD\RateableEntitiesBundle\Interfaces\Rateable;
+use ERD\RateableEntitiesBundle\Interfaces\RateableFactor;
 /**
  * Updates all rateable entities when one managed entity is persisted or updated.
  * 
@@ -19,78 +21,86 @@ class RateableSubscriber implements \Doctrine\Common\EventSubscriber
     
     /** @var Registry */
     protected $doctrineRegistry;
-    
+
     /**
      * Constructor
-     * 
-     * @param DoctrineEntityProvider A provider that returns all the rateable entities.
+     *
+     * @param Registry $doctrineRegistry Doctrine.
+     * @param DoctrineEntityProvider $provider A provider that returns all the rateable entities.
      */
     public function __construct(Registry $doctrineRegistry, DoctrineEntityProvider $provider)
     {
         $this->doctrineRegistry = $doctrineRegistry;
         $this->provider = $provider;
     }
-    
+
+    /**
+     * @todo Faster with onFlush?
+     */
     public function getSubscribedEvents()
     {
-        return array(Events::onFlush);
+        return array(Events::preUpdate, Events::prePersist, Event::preRemove);
+    }
+
+    public function prePersist(LifecycleEventArgs $eventArgs)
+    {
+        //If we're persisting an element in the rating system we always have
+        //to update all the ratings because it's likely that our new element
+        //changed how much they've decayed.
+        if($this->isRateable($eventArgs->getEntity()))
+        {
+            $this->updateRatings();
+        }
+    }
+
+    public function preRemove(LifecycleEventArgs $eventArgs)
+    {
+        //If we're removing an element in the rating system we always have
+        //to update all the ratings because it's likely that our new element
+        //changed how much they've decayed.
+        if($this->isRateable($eventArgs->getEntity()))
+        {
+            $this->updateRatings();
+        }
+    }
+
+    public function preUpdate(\Doctrine\ORM\Event\PreUpdateEventArgs $eventArgs)
+    {
+        $entity = $eventArgs->getEntity();
+        $changeSet = $eventArgs->getEntityChangeSet();
+
+        /* This condition updates the object if it's rateable and something related to its rating has changed.
+         *
+         * @todo Right now, the second part of the condition (something about its rating has changed) is always
+         * evaluated to true. This is because not only are the literal properties (relevanceToUser etc) related to the
+         * rating, but so are the results of the getRelevantContentAge() and getBusinessValue() methods...and there's
+         * no easy way to see if those results have changed--Doctrine only tells us what properties have changed, and
+         * we don't know what properties those methods depend on. So we have two options:
+         *
+         *  1) a config setting specifying which properties trigger update (probably a good idea anyway so the basic
+         *     rating properties can be renamed without causing problems.
+         *
+         *  2) We could clone the modified entity, reset all its changed properties with reflection, call the methods
+         *     on the "reverted" clone to get their original values and then call them on the entity in its current
+         *     state to see if they've changed.
+         */
+        if($this->isRateable($entity) &&
+           (count(array_intersect(array('relevanceToUser', 'decayRate', 'businessValue'), array_keys($changeSet))) > 0) || (1==1))
+        {
+            $this->updateRatings($entity);
+        }
+    }
+
+    protected function isRateable($entity)
+    {
+       return ($entity instanceof Rateable && !$entity instanceof RateableFactor);
     }
 
     /**
-     * Updates the ratings of all entities (from the provider) onFlush.
-     * 
-     * Must update on flush because updating prePersist/preUpdate would cause a loop (at least 
-     * with the standard $em->persist, though maybe not with the UOW API) as the elements whose
-     * ratings we're updating would themselves have to be persisted, again triggering all 
-     * elements to be updated and persisted. OnFlush prevents this because it's always the last
-     * thing to occur, and happens at the transaction/EM, rather than entity, level.
-     * 
-     * OnFlush may also have performance gains. One problem with it though is that it requires
-     * working with the UOW API directly to save changes, which is slightly more complicated. 
-     * 
-     * But the real complication comes in because we can have multiple entity managers, but only
-     * one is being flushed... So we need to update all its Rateable entities with UOW, but also 
-     * find the other EMs and update their Rateable entities but through the standard 
-     * EM->persist() method.
-     * 
-     * @param OnFlushEventArgs $eventArgs 
+     * @param $updateArgs The entity we're updating with this event, if any. Needed because Doctrine handles making
+     * changes to updated elements in a listener differently than it does making changes to new elements.
      */
-    public function onFlush(OnFlushEventArgs $eventArgs)
-    {
-        $entities  = $this->getRatedEntities();
-        $flushedEm = $eventArgs->getEntityManager();
-        $flushedEmClasses = $ourEm->getConfiguration()->getMetadataDriverImpl()->getAllClassNames();
-
-        //if we only have one em, so no need to detect which em each element belongs to. Much faster.
-        if(count($this->doctrineRegistry->getEntityManagers())==1)
-        {
-            foreach($entities as $entity)
-            {
-                //persist through UOW API
-            }            
-        }
-        
-        else
-        {
-            foreach($entities as $entity)
-            {
-                $class = get_class($entity);
-                
-                if(in_array($class, $flushedEmClasses))
-                {
-                    //persist with UOW API
-                }
-            
-                else
-                {
-                    $em = $this->doctrineRegistry->getEntityManagerForClass($class);
-                    $em->persist($entity); //will actually be applied when this EM ($em) is flushed.
-                }
-            }
-        }
-    }
-
-    protected function getRatedEntities()
+    protected function updateRatings(\Doctrine\ORM\Event\PreUpdateEventArgs $updateArgs = null)
     {
         $entities = $this->provider->getAllEntities();
         
@@ -102,11 +112,29 @@ class RateableSubscriber implements \Doctrine\Common\EventSubscriber
                 if($arca==$brca) { return 0; }
                 return ($arca > $brca) ? -1 : 1;
             });
-        
-        foreach($entities as $numberPublishedSince=>$entity)
+
+        if(!$updateArgs)
         {
-            $entity->setCurrentRating($numberPublishedSince);
-            //$this->doctrineRegistry->getEntityManagerForClass(get_class($entity))->persist($entity);
+            foreach($entities as $numberPublishedSince=>$entity)
+            {
+                $entity->setCurrentRating($numberPublishedSince);
+                //$this->doctrineRegistry->getEntityManagerForClass(get_class($entity))->persist($entity);
+            }
+        }
+
+        else
+        {
+            $updatingEntity = $updateArgs->getEntity();
+
+            foreach($entities as $numberPublishedSince=>$entity)
+            {
+                $entity->setCurrentRating($numberPublishedSince);
+
+                if($entity==$updatingEntity) {
+                    $newRating = $entity->getCurrentRating();
+                    $updateArgs->setNewValue('currentRating', $newRating);
+                }
+            }
         }
         
         return $entities;
