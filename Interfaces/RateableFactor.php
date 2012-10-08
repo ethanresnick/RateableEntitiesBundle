4725434626f83f6f66eb8cdf9f66c8b269190346
Ethan Resnick <ethan.resnick@gmail.com>
2012-10-08 11:34:25 -0400

    CS fixes

--- Interfaces/RateableFactor.php
+++ Interfaces/RateableFactor.php
@@ -8,14 +8,12 @@ namespace ERD\RateableEntitiesBundle\Interfaces;
  *
  * @author Ethan Resnick Design <hi@ethanresnick.com>
  * @copyright 2011 Ethan Resnick Design
- *
+ * 
  */
 interface RateableFactor {
-
+    
     public function getBusinessValue();
-
-    /**
-     * @param decimal $businessValue Set as -.5 >= x <= .5, with 0 being the site average.
-     */
-    public function setBusinessValue($businessValue);
+    
+    /** @param decimal $businessValue Set as -.5 >= x <= .5, with 0 being the site average. */
+    public function setBusinessValue($relevance);
 }
